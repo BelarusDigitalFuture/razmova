a8ed60d3af9b634f01fc7718cfd0b9a9388a99a6
5tronciy <62604867+5tronciy@users.noreply.github.com>
2021-02-09 13:20:27 +0300

    add MenuBar

--- spa/src/index.js
+++ spa/src/index.js
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 
 import { App } from "./App";
 import { store } from "./helpers";
+import "./index.css";
 
 import { configureFakeBackend } from "./helpers";
 configureFakeBackend();
