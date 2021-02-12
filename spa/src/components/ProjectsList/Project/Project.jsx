4587581ca51604219acf72105f8251aad8cbc298
5tronciy <62604867+5tronciy@users.noreply.github.com>
2021-02-12 15:54:49 +0300

    disc link

--- spa/src/components/ProjectsList/Project/Project.jsx
+++ spa/src/components/ProjectsList/Project/Project.jsx
@@ -1,36 +1,40 @@
+import { NavLink } from "react-router-dom";
+
 import styles from "./Project.module.css";
 import { ReactComponent as ChatCircleText } from "../../../assets/icons/ChatCircleTextgrey.svg";
 import { ReactComponent as Alarm } from "../../../assets/icons/Alarm.svg";
 
 const Project = ({ item }) => {
   return (
-    <div className={styles.project} key={item.title + item.id}>
-      <div>
-        <div className={styles.code}>{item.code}</div>
-      </div>
-      <div className={styles.description}>
-        <div className={styles.top}>
-          <div className={styles.title}>{item.title}</div>
-          <div className={styles.comments}>
-            <ChatCircleText />
-            {item.commentsQty}
+    <NavLink exact to="/discussion/3">
+      <div className={styles.project} key={item.title + item.id}>
+        <div>
+          <div className={styles.code}>{item.code}</div>
+        </div>
+        <div className={styles.description}>
+          <div className={styles.top}>
+            <div className={styles.title}>{item.title}</div>
+            <div className={styles.comments}>
+              <ChatCircleText />
+              {item.commentsQty}
+            </div>
+          </div>
+          <div className={styles.bottom}>
+            <div className={styles.tags}>
+              {item.tags.map((tag) => (
+                <div className={styles.tag} key={tag.title}>
+                  {tag.title}
+                </div>
+              ))}
+            </div>
+            <div className={styles.alarm}>
+              <Alarm />
+              {item.time}
+            </div>
           </div>
         </div>
-        <div className={styles.bottom}>
-          <div className={styles.tags}>
-            {item.tags.map((tag) => (
-              <div className={styles.tag} key={tag.title}>
-                {tag.title}
-              </div>
-            ))}
-          </div>
-          <div className={styles.alarm}>
-            <Alarm />
-            {item.time}
-          </div>
-        </div>
       </div>
-    </div>
+    </NavLink>
   );
 };
 
